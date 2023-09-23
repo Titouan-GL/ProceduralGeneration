7185c2f7409a4792e5985cb16cec711861d49510
titouan <titouan.guibertlode@free.fr>
2023-09-23 18:31:45 +0200

    added struct and the array for the map

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
-    [SerializeField] private float width;
-    [SerializeField] private float height;
+    public struct Tile
+    {
+        public Vector3 position;
+        public string type { get; set; }
+        public Tile(string newtype, Vector3 newposition)
+        {
+            type = newtype;
+            position = newposition;
+        }
+    }
+
+
+    [SerializeField] private int width;
+    [SerializeField] private int height;
 
     [SerializeField] private float tileWidth;
 
@@ -15,9 +27,12 @@ public class TerrainGenerator : MonoBehaviour
 
     private float tileRadius;
 
+    private Tile[][] map;
+
     private void Awake()
     {
         tileRadius = tileWidth / Mathf.Cos(Mathf.Deg2Rad * 30); // a side is : "adjacent/cos(30)" and this times two gives the radius, and tileWidth/2 = adjacent
+        map = new Tile[height][];
     }
 
     void Start()
@@ -25,12 +40,25 @@ public class TerrainGenerator : MonoBehaviour
         for (int i = 0; i < height; i++)
         {
             float offset = i % 2 == 1 ? tileWidth / 2 : 0;
+            Tile[] mapPart = new Tile[width];
 
             for (int j = 0; j < width; j++)
             {
-                Instantiate(grassTile, transform.position + new Vector3(j * tileWidth + offset, 0, i * tileRadius * 0.75f), Quaternion.Euler(new Vector3(-90, 0, 0)));
+                mapPart[j] = new Tile("grass", new Vector3(j * tileWidth + offset, 0, i * tileRadius * 0.75f));
+            }
+
+            map[i] = mapPart;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Instantiate(grassTile, map[i][j].position, Quaternion.Euler(new Vector3(-90, 0, 0)));
             }
         }
     }
+
+
 
 }
